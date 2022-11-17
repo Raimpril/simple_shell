8e84ac6b5a9456a2508f43bc2c751615c3160706
Awodire Mayowa <mawodire@gmail.com>
2022-11-17 04:56:51 +0100

    updated parser

--- parser.c
+++ parser.c
@@ -4,9 +4,9 @@
  * is_cmd - determines if a file is an executable command
  * @info: the info struct
  * @path: path to the file
- *
  * Return: 1 if true, 0 otherwise
  */
+
 int is_cmd(info_t *info, char *path)
 {
 	struct stat st;
@@ -27,9 +27,9 @@ int is_cmd(info_t *info, char *path)
  * @pathstr: the PATH string
  * @start: starting index
  * @stop: stopping index
- *
  * Return: pointer to new buffer
  */
+
 char *dup_chars(char *pathstr, int start, int stop)
 {
 	static char buf[1024];
@@ -47,9 +47,9 @@ char *dup_chars(char *pathstr, int start, int stop)
  * @info: the info struct
  * @pathstr: the PATH string
  * @cmd: the cmd to find
- *
  * Return: full path of cmd if found or NULL
  */
+
 char *find_path(info_t *info, char *pathstr, char *cmd)
 {
 	int i = 0, curr_pos = 0;
